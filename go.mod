7001192476dee423e695abba737bae7a0dc5a8f8
Shinnosuke Sawada <6warashi9@gmail.com>
2021-10-31 14:23:03 +0900

    feat: implement main

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/Warashi/esacli
+
+go 1.17
+
+require (
+	github.com/goccy/go-yaml v1.9.4
+	github.com/hori-ryota/esa-go v0.0.0-20171001110724-a41514de2edf
+)
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.0.0-20211030160813-b3129d9d1021 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/goccy/go-yaml"
+	"github.com/hori-ryota/esa-go/esa"
+)
+
+func main() {
+	if err := _main(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func _main() error {
+	var teamName, filename string
+	flag.StringVar(&teamName, "team", "", "team name. ex) warashi for warashi.esa.io")
+	flag.StringVar(&filename, "file", "", "filename")
+	flag.Parse()
+
+	token := os.Getenv("ESA_API_TOKEN")
+	client := esa.NewClient(token, teamName)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	body, meta, err := parseFile(filename)
+	if err != nil {
+		return err
+	}
+
+	if meta.Number == 0 {
+		meta, err := create(ctx, client, body, meta)
+		if err != nil {
+			return err
+		}
+		return writeFile(filename, body, meta)
+	}
+	meta, err = update(ctx, client, body, meta)
+	if err != nil {
+		return err
+	}
+	return writeFile(filename, body, meta)
+}
+
+func parseFile(filename string) (body string, meta *Meta, err error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return "", nil, fmt.Errorf("os.Open: %w", err)
+	}
+	defer f.Close()
+	return ParseFile(f)
+}
+
+func writeFile(filename string, body string, meta *Meta) error {
+	f, err := os.CreateTemp("", "")
+	tmpName := f.Name()
+	if err != nil {
+		return fmt.Errorf("os.Create: %w", err)
+	}
+	if err := write(f, body, meta); err != nil {
+		return fmt.Errorf("write: %w", err)
+	}
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("f.Close: %w", err)
+	}
+	if err := os.Rename(tmpName, filename); err != nil {
+		return fmt.Errorf("os.Rename: %w", err)
+	}
+	return nil
+}
+
+func write(w interface {
+	io.Writer
+	io.StringWriter
+}, body string, meta *Meta) error {
+	if _, err := w.WriteString("---\n"); err != nil {
+		return fmt.Errorf("w.WriteString: %w", err)
+	}
+	b, err := yaml.Marshal(meta)
+	if err != nil {
+		return fmt.Errorf("yaml.Marshal: %w", err)
+	}
+	if _, err := w.Write(b); err != nil {
+		return fmt.Errorf("w.Write: %w", err)
+	}
+	if _, err := w.WriteString("---\n"); err != nil {
+		return fmt.Errorf("w.WriteString: %w", err)
+	}
+	if _, err := w.WriteString(body); err != nil {
+		return fmt.Errorf("w.WriteString: %w", err)
+	}
+	return nil
+}
+
+func create(ctx context.Context, client esa.Client, body string, meta *Meta) (*Meta, error) {
+	tags := strings.Split(meta.Tags, ",")
+	for i := range tags {
+		tags[i] = strings.TrimSpace(tags[i])
+	}
+	p := esa.CreatePostParam{
+		Name:     meta.Title,
+		BodyMD:   &body,
+		Tags:     &tags,
+		Category: &meta.Category,
+		WIP:      esa.BoolP(!meta.Published),
+	}
+	post, err := client.CreatePost(ctx, p)
+	if err != nil {
+		return nil, fmt.Errorf("client.CreatePost: %w", err)
+	}
+	return &Meta{
+		Title:     post.Name,
+		Category:  post.Category,
+		Tags:      strings.Join(post.Tags, ","),
+		Published: !post.WIP,
+		Number:    post.Number,
+	}, nil
+}
+
+func update(ctx context.Context, client esa.Client, body string, meta *Meta) (*Meta, error) {
+	tags := strings.Split(meta.Tags, ",")
+	for i := range tags {
+		tags[i] = strings.TrimSpace(tags[i])
+	}
+	p := esa.UpdatePostParam{
+		Name:     &meta.Title,
+		BodyMD:   &body,
+		Tags:     &tags,
+		Category: &meta.Category,
+		WIP:      esa.BoolP(!meta.Published),
+	}
+	post, err := client.UpdatePost(ctx, meta.Number, p)
+	if err != nil {
+		return nil, fmt.Errorf("client.CreatePost: %w", err)
+	}
+	return &Meta{
+		Title:     post.Name,
+		Category:  post.Category,
+		Tags:      strings.Join(post.Tags, ","),
+		Published: !post.WIP,
+		Number:    post.Number,
+	}, nil
+
+}
